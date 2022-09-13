21f2158cb57c4c59cbe06f89c98baca05160aa4e
Alain <alain@pro.local>
2022-09-13 17:56:21 -0500

    Send transactions through client

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,16 +7,37 @@
 	let network = null;
 	let balance = null;
 	let isConnected = false;
+	let provider = null;
 
 	const contractAddress = import.meta.env.VITE_CONTRACT_ADDRESS;
 	let contract = null;
 	let allTips = [];
+	let sendingTip = false;
+
+	async function sendTip(event) {
+		sendingTip = true;
+		const formData = new FormData(event.target);
+		const data = {};
+		for (let field of formData) {
+			const [key, value] = field;
+			data[key] = value;
+		}
+
+		console.log(data);
+		const rwContract = new ethers.Contract(contractAddress, TipJarABI.abi, provider.getSigner());
+		const tx = await rwContract.sendTip(data.message, data.name, {
+			value: ethers.utils.parseEther(data.amount)
+		});
+		await tx.wait();
+	}
 
 	async function setupContract() {
 		if (isConnected) {
-			contract = new ethers.Contract(contractAddress, TipJarABI.abi.provider);
+			contract = new ethers.Contract(contractAddress, TipJarABI.abi, provider.getSigner());
 			contract.on('NewTip', async () => {
 				balance = await provider.getBalance(userAddress);
+				await getAllTips();
+				sendingTip = false;
 			});
 		}
 	}
@@ -24,22 +45,27 @@
 	async function getAllTips() {
 		if (isConnected) {
 			const tips = await contract.getAllTips();
-			allTips = tips.map((item) => {
-				return {
-					...item,
-					timestamp: new Date(item.timestamp * 1000)
-				};
-			});
+			allTips = [
+				...tips.map((item) => {
+					return {
+						...item,
+						timestamp: new Date(item.timestamp * 1000),
+						amount: ethers.utils.formatEther(item.amount.toString())
+					};
+				})
+			];
 		}
 	}
 
 	async function setup(accounts) {
 		userAddress = accounts[0];
 		try {
-			const provider = new ethers.providers.Web3Provider(window.ethereum);
+			provider = new ethers.providers.Web3Provider(window.ethereum);
 			network = await provider.getNetwork();
 			balance = await provider.getBalance(userAddress);
 			isConnected = true;
+			await setupContract();
+			await getAllTips();
 		} catch (error) {
 			console.error(error);
 		}
@@ -78,6 +104,29 @@
 		<li>Current balance: {ethers.utils.formatEther(balance)}</li>
 	</ul>
 
+	<form
+		class="w-2/3 mx-auto border rounded-md border-indigo-200 flex flex-col gap-8 p-6 mt-4"
+		on:submit|preventDefault={sendTip}
+	>
+		<div class="grid grid-cols-2">
+			<label for="amount">Send me an ETH tip!</label>
+			<input name="amount" placeholder="0.001" />
+		</div>
+		<div class="grid grid-cols-2">
+			<label for="name">Your name</label>
+			<input name="name" />
+		</div>
+		<div class="grid grid-cols-2">
+			<label for="message">Your message</label>
+			<input name="message" />
+		</div>
+		<button
+			disabled={sendingTip}
+			type="submit"
+			class="bg-green-500 text-gray-50 shadow-md rounded-md px-2 py-2 text-center w-1/3"
+			>Send a tip</button
+		>
+	</form>
 	<table class="mt-8 border-collapse table-auto w-2/3 mx-auto text-sm h-80 overflow-auto">
 		<thead>
 			<tr>
