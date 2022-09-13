143b935c1c865b9721e1f0cd23f1cdda62f501e5
Alain <alain@pro.local>
2022-09-13 17:11:33 -0500

    Configure client

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,7 @@
-<script>import "../app.postcss";</script><slot></slot>
+<script>
+	import '../app.postcss';
+</script>
+
+<div class="w-full h-screen bg-gray-200 flex items-center flex-col p-32">
+	<slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,29 @@
 <script>
+	import { onMount } from 'svelte';
+	import { ethers } from 'ethers';
+
 	let userAddress = null;
+	let network = null;
+	let balance = null;
+	let isConnected = false;
+
+	async function setup(accounts) {
+		userAddress = accounts[0];
+		try {
+			const provider = new ethers.providers.Web3Provider(window.ethereum);
+			network = await provider.getNetwork();
+			balance = await provider.getBalance(userAddress);
+			isConnected = true;
+		} catch (error) {
+			console.error(error);
+		}
+	}
 
 	async function connectWallet() {
 		if (window.ethereum) {
 			const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
 			if (accounts.length > 0) {
-				userAddress = accounts[0];
+				await setup(accounts);
 			} else {
 				alert('No accounts found');
 			}
@@ -13,12 +31,26 @@
 			alert('No ethereum wallet found');
 		}
 	}
+
+	onMount(async () => {
+		if (window.ethereum) {
+			const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+			if (accounts.length > 0) {
+				await setup(accounts);
+			}
+		}
+	});
 </script>
 
-{#if userAddress}
+{#if isConnected}
 	<p class="text-xl text-green-600">
 		Successfully connected to account <strong>{userAddress}</strong>
 	</p>
+
+	<ul>
+		<li>Current network: {network.name}</li>
+		<li>Current balance: {ethers.utils.formatEther(balance)}</li>
+	</ul>
 {:else}
 	<button class="bg-blue-600 text-gray-50 shadow-md rounded-md px-3 py-8" on:click={connectWallet}>
 		Connect with wallet
